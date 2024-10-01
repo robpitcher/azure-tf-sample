288e5888a3da7ba91404bba617a650e56974768c
robpitcher <13648061+robpitcher@users.noreply.github.com>
2024-09-30 21:22:04 -0400

    chore: move providers

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.3.0"
-    }
-  }
-
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "rg-tf-samples"
-    storage_account_name = "fdsafewafg34"
-    container_name       = "tfstates"
-    key                  = "sample.tfstate"
-    use_oidc             = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
-}
-
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
