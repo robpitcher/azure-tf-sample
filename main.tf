2b770f0d55fa8ea160d87106c4527770c844597d
robpitcher <13648061+robpitcher@users.noreply.github.com>
2024-10-01 07:38:24 -0400

    feat: simplifying test env

--- main.tf
+++ main.tf
@@ -1,27 +1,4 @@
-# Get the current client configuration from the AzureRM provider.
-# This is used to populate the root_parent_id variable with the
-# current Tenant ID used as the ID for the "Tenant Root Group"
-# management group.
-
-data "azurerm_client_config" "core" {}
-
-# Declare the Azure landing zones Terraform module
-# and provide a base configuration.
-
-module "enterprise_scale" {
-  source = "git::https://github.com/Azure/terraform-azurerm-caf-enterprise-scale.git?ref=dc0c89d9fe7b89314002ba234c7f2b38fbc896b9"
-  # version = "6.1.0" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
-
-  default_location = var.default_location
-
-  providers = {
-    azurerm              = azurerm
-    azurerm.connectivity = azurerm
-    azurerm.management   = azurerm
-  }
-
-  root_parent_id = "roblabs"
-  root_id        = var.root_id
-  root_name      = var.root_name
-
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
 }
