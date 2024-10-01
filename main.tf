405c70e60bba33ce0604a4d33772067860e4508f
robpitcher <13648061+robpitcher@users.noreply.github.com>
2024-09-30 21:27:41 -0400

    feat: es tf module

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
-# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
-  location = var.location
+# Get the current client configuration from the AzureRM provider.
+# This is used to populate the root_parent_id variable with the
+# current Tenant ID used as the ID for the "Tenant Root Group"
+# management group.
+
+data "azurerm_client_config" "core" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "6.1.0" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  default_location = var.default_location
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,16 @@
 variable "resource_group_name" {}
 variable "location" {}
+
+variable "root_id" {
+  type    = string
+  default = "es"
+}
+
+variable "root_name" {
+  type    = string
+  default = "Enterprise-Scale"
+}
+
+variable "default_location" {
+  type    = string
+}
